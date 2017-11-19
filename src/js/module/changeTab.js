b327f8b9ba1b1d42877e2feb6da6a6eb91d635a3
rnaxu <mayumiii.seki@gmail.com>
2017-11-20 01:48:23 +0900

    Add tab

---
+++ src/js/module/changeTab.js
@@ -0,0 +1,19 @@
+export default () => {
+  const tabItems = document.getElementsByClassName('js-tab-trigger');
+
+  function changeTab(e) {
+    e.preventDefault();
+
+    // 一旦全部非活性に
+    Array.prototype.forEach.call(tabItems, (el) => {
+      el.classList.remove('is-active');
+    });
+
+    // 選択されたタブだけ活性に
+    e.currentTarget.classList.add('is-active')
+  }
+
+  Array.prototype.forEach.call(tabItems, (el) => {
+    el.addEventListener('click', changeTab);
+  })
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,4 @@
 import svg4everybody from 'svg4everybody';
+import changeTab from './module/changeTab'
 svg4everybody();
+changeTab();
